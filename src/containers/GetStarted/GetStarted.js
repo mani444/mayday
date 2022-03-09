93ccc87cbccf52b6584b71f9f2f4716d0c03170b
mani444 <manijan444@gmail.com>
2022-03-09 21:09:23 +0500

    Added my project

--- src/containers/GetStarted/GetStarted.js
+++ src/containers/GetStarted/GetStarted.js
@@ -1,29 +1,67 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faCheck } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useState, useMemo, useRef } from "react";
 import styles from "./GetStarted.module.scss";
 
 import Button from "../../components/UI/Button/Button";
+// const kvArray = [
+//   { key: 1, value: 10 },
+//   { key: 2, value: 20 },
+//   { key: 3, value: 30 },
+// ];
+
+// const strings = ["10", "10", "10"];
+// const numbers = strings.map((str) => {
+//   return parseInt(str);
+// });
+
+// console.log(numbers);
+// const reformattedArray = kvArray.map((key) => {
+//   console.log(key.key);
+//   return key.value;
+// });
+
+// console.log("reformattedArray " + reformattedArray);
 
 const Getstarted = () => {
   const back = <FontAwesomeIcon icon={faArrowLeft} />;
   const checkbox = <FontAwesomeIcon icon={faCheck} color="#00ff00" />;
-  // const [count, setCount] = useState(0);
-  const [check, setChecked] = useState({
-    checked1: false,
-    checked2: false,
-    checked3: false,
-  });
-  console.log(check.checked1);
-  console.log(check.checked2);
+  const [check, setChecked] = useState([false, false, false]);
+  const checkboxes = useRef([false, false, false]);
 
-  console.log(check.checked3);
-  for (let checked in check) {
-    const item = check[checked];
-    console.log(item);
-  }
+  const handleChange = (index) => {
+    const arr = [...check];
+    arr[index] = !arr[index];
+    setChecked(arr);
+  };
+  const handleRefChange = (index) => {
+    checkboxes.current[index] = !checkboxes.current[index];
+    console.log("checkboxes.current[index]", checkboxes.current[index]);
+  };
+
+  const checkRefEnable = (check1) => {
+    console.log("val ", check1);
+
+    for (let i = 0; i < check1.length; i++) {
+      if (!check1[i]) return false;
+    }
+
+    return true;
+  };
+
+  const checkEnable = (check1) => {
+    // console.log("val ", check1);
+
+    for (let i = 0; i < check1.length; i++) {
+      if (!check1[i]) return false;
+    }
+
+    return true;
+  };
   return (
     <div className={styles.Topbox}>
+      {}
+
       <h4>GET STARTED</h4>
       <p>
         Make sure you've set up a video call and everyone has joined, in case
@@ -37,26 +75,22 @@ const Getstarted = () => {
       <div className={styles.Rules}>
         <div
           onClick={() => {
-            setChecked((prevState) => ({
-              ...prevState,
-              checked1: !check.checked1,
-            }));
+            handleChange(0);
+            handleRefChange(0);
           }}
         >
-          {check.checked1 ? <div> {checkbox}</div> : null}
+          {check[0] ? <div> {checkbox}</div> : null}
         </div>
         <label>All players in my team are logged in and ready to start</label>
         <br />
         <br />
         <div
           onClick={() => {
-            setChecked((prevState) => ({
-              ...prevState,
-              checked2: !check.checked2,
-            }));
+            handleChange(1);
+            handleRefChange(1);
           }}
         >
-          {check.checked2 ? <div> {checkbox}</div> : null}
+          {check[1] ? <div> {checkbox}</div> : null}
         </div>
         <label>
           All players in my team are on the same video call or are physically
@@ -66,20 +100,18 @@ const Getstarted = () => {
         <br />
         <div
           onClick={() => {
-            setChecked((prevState) => ({
-              ...prevState,
-              checked3: !check.checked3,
-            }));
+            handleChange(2);
+            handleRefChange(2);
           }}
         >
-          {check.checked3 ? <div> {checkbox}</div> : null}
+          {check[2] ? <div> {checkbox}</div> : null}
         </div>
         <label>
           I understand that pressing the Start Game button starts the game for
           everyone
         </label>
       </div>
-
+      <div></div>
       <div className="StartArea">
         <Button
           classes="StartButtons backbutton"
@@ -88,7 +120,8 @@ const Getstarted = () => {
         />
         <Button
           classes={
-            check.checked1 && check.checked2 && check.checked3
+            // useMemo(() => checkEnable(check), [check])
+            checkRefEnable(checkboxes.current)
               ? "StartButtons startbuttonactive"
               : "StartButtons startbutton"
           }
